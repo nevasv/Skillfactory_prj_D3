f9d8c190e1bf026cb9c21098eaee4bf047b0c5df
vladmspb <nevasv@yandex.ru>
2023-02-15 23:18:00 +0300

    Модели про студентов и курсы

--- prj/djangoprj/admin.py
+++ prj/djangoprj/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from djangoprj.models import Category, Good
+from djangoprj.models import Category, Good, Course, Student
 
 admin.site.register(Category)
 admin.site.register(Good)
+admin.site.register(Course)
+admin.site.register(Student)
