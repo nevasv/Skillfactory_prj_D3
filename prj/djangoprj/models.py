d01ec0fd95f385efe68e2d5219b310ea4fa56c9a
vladmspb <nevasv@yandex.ru>
2023-02-15 22:09:29 +0300

    Meta

--- prj/djangoprj/models.py
+++ prj/djangoprj/models.py
@@ -2,8 +2,13 @@ from django.db import models
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=30, unique=True)
-    descriptions = models.TextField()
+    name = models.CharField(max_length=30, unique=True, verbose_name= 'Название')
+    descriptions = models.TextField(null=True, blank=True, verbose_name= 'Описание')  # поле может быть пустым
 
     def __str__(self):
         return self.name
+
+
+class Meta:
+    verbose_name = 'Категория'
+    verbose_name_plural = 'Категории'
